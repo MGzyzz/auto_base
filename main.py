64d61262e514ab0a5ff7a8c1c5f518062e202207
Madi <madi_gaziz@mail.ru>
2023-08-19 18:26:09 +0400

    1 Этап: Создал класс Truck для обработки данных и TruckManagement для обработки запросов(Загрузки данных), а так же реализовал try для проверки на то что файл dump.json есть по указанному пути. Так же создал файл Main для проверки на то что проверки работоспособности класса

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+
+
+class Truck:
+    def __init__(self, id, name, driver, state_name):
+        self.id = id
+        self.name = name
+        self.driver = driver
+        self.state_name = state_name
+
+
+class TruckManagement:
+    def __init__(self):
+        self.trucks = []
+        self.load_data()
+
+    def load_data(self):
+        try:
+            with open("dump.json", "r") as file:
+                data = json.load(file)
+                for item in data:
+                    truck = Truck(item["id"], item["name"], item["driver"], item["state_name"])
+                    self.trucks.append(truck)
+        except FileNotFoundError:
+            print("Файл не найден. Создайте файл trucks.json с данными.")
+
+
+
+class Main:
+    while True:
+        if TruckManagement:
+            print('Work')
+            break
